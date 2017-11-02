bd565bd1879a88d03a723796053ff9ceec5cc527
kaushal0 <kaushalgadhia@gmail.com>
2017-11-02 02:51:32 +0000

    Adding threads and checking join chatroom

--- ClientCode.py
+++ ClientCode.py
@@ -2,40 +2,58 @@
 
 import socket
 
+def join():
+	chatroom = input('Enter Chatroom Name to Enter : ')
+	Cname = input('\nEnter Client Name : ')
+
+	conn_msg = "JOIN_CHATROOM:".encode('utf-8') + chatroom.encode('utf-8') + "\n".encode('utf-8')
+	conn_msg += "CLIENT IP: \n".encode('utf-8')
+	conn_msg += "PORT: \n".encode('utf-8')
+	conn_msg += "CLIENT_NAME:".encode('utf-8') + Cname.encode('utf-8') + "\n".encode('utf-8')
+
+s.send(conn_msg)
+
+
+
 # creating the socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # get local machine name
 host = input('Enter the Hostname : ')
-port = 8000
+port = input('Port') 
+
 # connection to hostname on the port.
 s.connect((host, port))
 
-chatroom = input('Enter Chatroom Name to Enter : ')
-Cname = input('\nEnter Client Name : ')
+Cname = input('Give Client Name')
+join()
+jID=0
+data = s.recv(1024)
+print(data.decode(encoding='utf-8'))
+p = data.find(b'JOINED')
+if p==0:
+	jID_start = data.find('JOIN_ID'.encode('utf-8'))+9
+	jID_end = data.find('\n'.encode('utf-8'),jID_start)-1
+	jID = str(data[jID_start:jID_end])
 
-conn_msg = "JOIN_CHATROOM:".encode('utf-8') + chatroom.encode('utf-8') + "\n".encode('utf-8')
-conn_msg += "CLIENT IP: \n".encode('utf-8')
-conn_msg += "PORT: \n".encode('utf-8')
-conn_msg += "CLIENT_NAME:".encode('utf-8') + Cname.encode('utf-8') + "\n".encode('utf-8')
+serverThread = Client(s)
+serverThread.start()
 
-s.send(conn_msg)
 
 while(1):
-	message = s.recv(1024)
 	print('Enter Option to choose:')
 	print('1. Join')
 	print('2. Chat')
 	print('3. Leave')
 	print('4. Disconnect')
-	task = input('')
-	if task == 1:
+	task = input('?')
+	if task == '1':
 		join()
-	elif task == 2:
+	elif task == '2':
 		chat()
-	elif task == 3:
+	elif task == '3':
 		leave()
-	elif task == 4:
+	elif task == '4':
 		discon()
-	elif task == 5:
+	elif task == '5':
 		print('Error')
