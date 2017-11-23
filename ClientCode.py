3c85f7f3df938ef6d0059baf36b57e64902cf7d8
kaushal0 <kaushalgadhia@gmail.com>
2017-11-23 22:05:42 +0000

    Thread and chat function

--- ClientCode.py
+++ ClientCode.py
@@ -1,18 +1,38 @@
 ## Client Code for Multiple Client Chat Server
 
 import socket
+import sys
+from threading import Thread
 
 def join():
 	chatroom = input('Enter Chatroom Name to Enter : ')
-	Cname = input('\nEnter Client Name : ')
+	Clname = input('\nEnter Client Name : ')
 
 	conn_msg = "JOIN_CHATROOM:".encode('utf-8') + chatroom.encode('utf-8') + "\n".encode('utf-8')
 	conn_msg += "CLIENT IP: \n".encode('utf-8')
 	conn_msg += "PORT: \n".encode('utf-8')
 	conn_msg += "CLIENT_NAME:".encode('utf-8') + Cname.encode('utf-8') + "\n".encode('utf-8')
+	s.send(conn_msg)
 
-s.send(conn_msg)
+def chat(socket):
+	croom = input('Which room to send message? ')
+	chat_message = input('Enter Message to send: ')
+	msg = "CHAT: ".encode('utf-8') + croom.encode('utf-8') + "\n".encode('utf-8')
+	msg += "JOIN_ID: ".encode('utf-8') + str(jID).encode('utf-8') + "\n".encode('utf-8')
+	msg += "CLIENT_NAME: ".encode('utf-8') + Clname.encode('utf-8') + "\n".encode('utf-8')
+	msg += "MESSAGE: ".encode('utf-8') + chat_message.encode('utf-8') + "\n\n".encode('utf-8')
+	s.send(msg)
 
+class client_thread(Thread):
+	def __init__(self, socket)
+		Thread.__init__(self)
+		self.socket= socket
+
+	def run(self):
+		while True:
+			data = self.socket.recv(1024)
+			print("Client Message : ")
+			print(data.decode(encoding='utf-8'))
 
 
 # creating the socket
@@ -20,12 +40,14 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # get local machine name
 host = input('Enter the Hostname : ')
-port = input('Port') 
+port = input('Port')
 
-# connection to hostname on the port.
+# connecting to host
 s.connect((host, port))
 
-Cname = input('Give Client Name')
+#Basic response test
+
+Clname = input('Give Client Name')
 join()
 jID=0
 data = s.recv(1024)
@@ -36,8 +58,8 @@ if p==0:
 	jID_end = data.find('\n'.encode('utf-8'),jID_start)-1
 	jID = str(data[jID_start:jID_end])
 
-serverThread = Client(s)
-serverThread.start()
+cThread = client_thread(s)
+cThread.start()
 
 
 while(1):
@@ -50,7 +72,8 @@ while(1):
 	if task == '1':
 		join()
 	elif task == '2':
-		chat()
+		print("Chat")
+		chat(s)
 	elif task == '3':
 		leave()
 	elif task == '4':
